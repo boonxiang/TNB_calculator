fe80a19f2a92dade5756c879768e70a98f349771
boonxiang <104239850+boonxiang@users.noreply.github.com>
2025-07-18 14:39:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,25 +24,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode()
-    
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/png;base64,{encoded_string}");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
 
-add_bg_from_local("backgroud_picture.png") 
 
 st.title("TNB Electricity Bill Calculator and Reverser")
 
